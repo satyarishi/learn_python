07dc205d7850f033dcc39d8fc552ee96b1c54fbc
SATYA PRAKASH KUMAR <satya3753@gmail.com>
2021-10-27 00:14:01 +0530

    some code added

--- oct21.py
+++ oct21.py
@@ -314,3 +314,25 @@ print(Min(32))
 """
 
 
+# Remove key:pair which not contain digit
+"""
+d ={'a': 'a', 'B':2,'1':3}
+d = {key: value for key,value in d.items() if key.isdigit()}
+print("Dictionary:",end="")
+print(d)
+"""
+# Remove values which not contain digiit
+"""
+d1 = {'A':'A','B':2,'1':3}
+d1 = {value for value in d1.values() if value.isdigit()}
+print("Dictionary:",end="")
+print(d1)
+"""
+
+# Remove keys which is not contain digit
+"""
+d2 = {'A':'A','B':2,'1':3}
+d2 = {key for key in d2.keys() if key.isdigit()}
+print("Dictionary:",end =" ")
+print(d2)
+"""
---
+++ wifi_qrcode.py
@@ -0,0 +1,4 @@
+import wifi_qrcode_generator as qr
+ 
+# Use wifi_qrcode() to create a QR image
+qr.wifi_qrcode('Airtel_9708541303', False, 'WPA', 'air19654')
